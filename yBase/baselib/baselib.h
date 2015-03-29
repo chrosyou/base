35f73d0efe6aacb66f1d358e04ea4e7fd70e56c9
chrosyou <646680299@qq.com>
2015-03-29 15:53:28 +0800

    test

--- yBase/baselib/baselib.h
+++ yBase/baselib/baselib.h
@@ -4,4 +4,4 @@
 #include "pathhelp\AnalyseCmdLine.h"
 #include "pathhelp\MsAnalyseCmdLine.h"
 
-#include "win32ulity/resource/ApplicationAttribute.h"
+#include "win32ulity\resource\ApplicationAttribute.h"
