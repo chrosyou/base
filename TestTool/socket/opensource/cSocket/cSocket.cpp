786df2ca4252b636e7b2755b40d194256d6c1424
chrosyou <646680299@qq.com>
2015-07-20 23:42:23 +0800

    cSocket

---
+++ TestTool/socket/opensource/cSocket/cSocket.cpp
@@ -0,0 +1,42 @@
+// cSocket.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Socket.h"
+
+#include <iostream>
+
+#pragma comment (lib, "Ws2_32.lib")
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	try {
+		SocketClient s("www.google.com", 80);
+
+		s.SendLine("GET / HTTP/1.0");
+		s.SendLine("Host: www.google.com");
+		s.SendLine("");
+
+		while (1) {
+			string l = s.ReceiveLine();
+			if (l.empty()) break;
+			cout << l;
+			cout.flush();
+		}
+
+	} 
+	catch (const char* s) {
+		cerr << s << endl;
+	} 
+	catch (std::string s) {
+		cerr << s << endl;
+	} 
+	catch (...) {
+		cerr << "unhandled exception\n";
+	}
+
+	return 0;
+}
+
