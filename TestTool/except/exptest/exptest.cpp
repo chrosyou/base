baf9627298115a192147eea61668eb1f1b12c0fa
chrosyou <646680299@qq.com>
2015-07-23 00:22:30 +0800

    trycatch

--- TestTool/except/exptest/exptest.cpp
+++ TestTool/except/exptest/exptest.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+double divtest(double a, double b)
+{
+	if (b - 0 < 0.000000001)
+	{
+		throw "argument is not right";
+	}
+
+	return a / b;
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	try
+	{
+		char *a = NULL;
+		*a = 'c';
+		//divtest(1, 0);
+	}
+	catch (...)
+	{
+		std::cout<<"except occur"<<std::endl;
+	}
 
 	return 0;
 }
